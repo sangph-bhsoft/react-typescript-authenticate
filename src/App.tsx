fd088f51403ca62e4468d80ff9c4447cf1ff5810
meocon200890 <meocon200890@gmail.com>
2020-12-02 13:34:33 +0700

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center h-screen">
+      <div className="bg-white-100   rounded px-8 pt-6 pb-8 mb-4 flex shadow w-2/5 p-8 flex-col items-center">
+        <h1 className="text-gray-darker  font-bold text-xl">LOGIN</h1>
+        <div className="mb-4 w-full">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Username
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 outline-none text-gray-700"
+            id="username"
+            type="text"
+            placeholder="username"
+          />
+        </div>
+        <div className="mb-4 w-full">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border border-red rounded w-full outline-none py-2 px-3 text-gray-700 mb-3"
+            id="password"
+            type="password"
+            placeholder="******************"
+          />
+          <p className="text-red-500 text-xs italic">
+            Please choose a password.
+          </p>
+        </div>
+        <div className="flex items-center justify-between w-full">
+          <button className="bg-purple-600 bg-opacity-75 hover:bg-opacity-100 text-white font-bold py-2 px-4 rounded focus:outline-none">
+            LOGIN
+          </button>
+          <a
+            className="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker"
+            href="/"
+          >
+            Register
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+      purple: colors.violet,
+      primary: "#5c6ac4",
+      secondary: "#ecc94b",
+      black: "#000",
+      white: "#fff",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      borderColor: ["focus-visible"],
+      opacity: ["disabled"],
+    },
+  },
+};
